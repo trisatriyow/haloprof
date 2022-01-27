e28de797117b746989317fc7c2b7a65e8a47d861
trisatriyow <triyonosatriyo15@gmail.com>
2022-01-28 02:16:12 +0700

    sampai appointment

--- app.js
+++ app.js
@@ -1,126 +1,104 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const {User} = require('./models')
-const nodemailer = require("nodemailer");
+const { User } = require("./models");
+
 const express = require("express");
 const Controller = require("./controllers");
 const app = express();
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const initializePassport = require('./passport-config')
-initializePassport(
-  passport,
-  email => User.findOne({where: {email:email}}),
-  id => User.findOne({where: {id}})
-  )
-  const methodOverride = require('method-override');
-  const port = 3000;
-  app.set("view engine", "ejs");
-  app.use(express.urlencoded({ extended: true }));
-  app.use(flash())
-  app.use(session({
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const initializePassport = require("./passport-config");
+const methodOverride = require("method-override");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
-  app.use(methodOverride('_method'))
-  app.get('/', checkAuthenticated,
-  Controller.userProfileList
-  )
-  app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
+    saveUninitialized: false,
   })
-  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.get("/", checkAuthenticated, Controller.userProfileList);
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+initializePassport(
+  passport,
+  (email) => User.findOne({ where: { email: email } }),
+  (id) => User.findOne({ where: { id } })
+);
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
   })
-  )
-  app.get('/register', 
-  checkNotAuthenticated, 
-  (req, res) => {
-    res.render('register.ejs')
-  })
-  app.post('/register', 
-  checkNotAuthenticated, 
-  (req, res) => {
-      const { password, name, email} = req.body
-      // console.log(password)
-      const hashedPassword = bcrypt.hash(req.body.password, 10)
-      .then((data)=>{
-        // console.log(data)
-        return User.create({
-          username: name,
-          email,
-          password: data
-      })
+);
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+app.post("/register", checkNotAuthenticated, (req, res) => {
+  const { password, name, email } = req.body;
+  // console.log(password)
+  const hashedPassword = bcrypt
+    .hash(req.body.password, 10)
+    .then((data) => {
+      // console.log(data)
+      return User.create({
+        username: name,
+        email,
+        password: data,
+      });
     })
-      .then((data)=>{
-        res.redirect('/login')
-      })
-      .catch((err)=>{
-        res.send(err)
-      })
-      .catch (()=>{
-        res.redirect('/register')
-      }) 
-  })
-  
-  app.get("/doctorList", Controller.doctorList);
-  app.get("/userProfileList", Controller.userProfileList);
-  app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-  })
-  
-  let localStorage;
-  if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require("node-localstorage").LocalStorage;
-    localStorage = new LocalStorage("./scratch");
-}
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "haloprofhacktiv8@gmail.com",
-    pass: "hacktiv8",
-  },
+    .then((data) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+    .catch(() => {
+      res.redirect("/register");
+    });
 });
 
-let mailOptions = {
-  from: "haloprofhacktiv8@gmail.com",
-  to: localStorage.getItem("email"),
-  subject: "Halo Prof Appointment",
-  text: "Confirmation about appointment Halo Prof",
-};
-
-transporter.sendMail(mailOptions, (err, info) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Email Sent:" + info.response);
-  }
+app.get("/doctorList", Controller.doctorList);
+app.get(
+  "/appointment/:UserId/doctors/:DoctorId",
+  Controller.confirmAppointment
+);
+app.get("/userProfileList", Controller.userProfileList);
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
 });
 
 async function checkAuthenticated(req, res, next) {
   // let {email, password} = req.body
-  const x= await req.isAuthenticated();
-  console.log(x)
+  const x = await req.isAuthenticated();
+  console.log(x);
   if (x) {
-    return next()
+    return next();
   }
-  res.redirect('/login')
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  next()
+  next();
 }
 
 app.listen(port, () => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,15 @@
 const { User, Docter, Profile, Appointment } = require("../models");
+const nodemailer = require("nodemailer");
+let memory = {};
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "haloprofhacktiv8@gmail.com",
+    pass: "hacktiv8",
+  },
+});
 
 class Controller {
-  
   static tabelApp(req, res) {
     Appointment.findAll({ include: [{ model: User }, { model: Doctor }] })
       .then((data) => {
@@ -12,8 +20,11 @@ class Controller {
         res.send(err);
       });
   }
+
   static doctorList(req, res) {
     const { spesialisasi } = req.query;
+    memory.UserId = 1;
+    memory.email = "triyonosatriyo15@gmail.com";
     let option = {
       order: [["count", "asc"]],
     };
@@ -22,7 +33,7 @@ class Controller {
     }
     Docter.findAll(option)
       .then((data) => {
-        res.render("docterList", { data });
+        res.render("docterList", { data, memory });
       })
       .catch((err) => {
         res.send(err);
@@ -36,6 +47,34 @@ class Controller {
     })
       .then((data) => {
         res.send(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+  static confirmAppointment(req, res) {
+    const { DoctorId, UserId } = req.params;
+    Profile.findAll({
+      include: {
+        model: Docter,
+      },
+    })
+      .then((data) => {
+        let mailOptions = {
+          from: "haloprofhacktiv8@gmail.com",
+          to: memory.email,
+          subject: "Halo Prof Appointment",
+          text: "Confirmation about appointment Halo Prof",
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Email Sent:" + info.response);
+          }
+        });
+        res.render("appointment", { data });
       })
       .catch((err) => {
         res.send(err);
