fa628362b317ca1bbf333432e3dda608eb88d738
trisatriyow <triyonosatriyo15@gmail.com>
2022-01-27 15:24:52 +0700

    appointment belum

---
+++ migrations/20220127080208-create-profile.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable("Profiles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      age: {
+        type: Sequelize.INTEGER,
+      },
+      gender: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable("Profiles");
+  },
+};
